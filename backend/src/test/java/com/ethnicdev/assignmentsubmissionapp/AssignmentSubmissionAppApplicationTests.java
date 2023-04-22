bf4bd198fc491e4168b91da7735e8b2c564f5771
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-04-22 13:10:24 +0700

    first commit

---
+++ backend/src/test/java/com/ethnicdev/assignmentsubmissionapp/AssignmentSubmissionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethnicdev.assignmentsubmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
