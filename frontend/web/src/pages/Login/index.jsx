444b9fc73d651b13bbd3a56519945d68a2da9220
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-05-06 11:06:55 +0700

    Connecting a ReactJS Login Page to a Spring RESTful Endpoint

--- frontend/web/src/pages/Login/index.jsx
+++ frontend/web/src/pages/Login/index.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import useLocalState from "../../util/useLocalStorage";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [, setJwt] = useLocalState("", "jwt");
+
+  const sendLoginRequest = () => {
+    const body = {
+      username: username,
+      password: password,
+    };
+
+    fetch("api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (response.status === 200)
+          return Promise.all([response.json(), response.headers]);
+        else return Promise.reject("Invalid login attemp");
+      })
+      .then(([body, headers]) => {
+        setJwt(headers.get("authorization"));
+        window.location.href = "dashboard";
+      })
+      .catch((message) => {
+        alert(message);
+      });
+  };
+
   return (
     <>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="email" id="username" />
+        <input
+          type="email"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div>
+        <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+          Submit
+        </button>
       </div>
     </>
   );
