1fa88d6887faa20cc120ee7b1252acf560042dd5
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-05-09 00:32:06 +0700

    Reducing Fetch call Boilerplate Code

--- frontend/web/src/pages/AssignmentView/index.jsx
+++ frontend/web/src/pages/AssignmentView/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useLocalState from "../../util/useLocalStorage";
+import ajax from "../../services/fetchService";
 
 export default function AssignmentView() {
   const params = useParams();
@@ -18,36 +19,20 @@ export default function AssignmentView() {
   };
 
   const save = () => {
-    fetch(`/api/assignments/${assignmentId}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-      method: "PUT",
-      body: JSON.stringify(assignment),
-    })
-      .then((response) => {
-        return response.status === 200 ? response.json() : null;
-      })
-      .then((assignmentData) => {
+    ajax(`/api/assignments/${assignmentId}`, "PUT", jwt, assignment).then(
+      (assignmentData) => {
+        console.log(assignmentData);
         setAssignment(assignmentData);
-      });
+      }
+    );
   };
 
   useEffect(() => {
-    fetch(`/api/assignments/${assignmentId}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-      method: "GET",
-    })
-      .then((response) => {
-        if (response.status === 200) return response.json();
-      })
-      .then((assignmentData) => {
+    ajax(`/api/assignments/${assignmentId}`, "GET", jwt).then(
+      (assignmentData) => {
         setAssignment(assignmentData);
-      });
+      }
+    );
   }, []);
 
   return (
--- frontend/web/src/pages/Dashboard/index.jsx
+++ frontend/web/src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useLocalState from "../../util/useLocalStorage";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import ajax from "../../services/fetchService";
 
 export default function Dashboard() {
   const [jwt, setJwt] = useLocalState("", "jwt");
@@ -24,19 +25,9 @@ export default function Dashboard() {
   }, []);
 
   const createAssignment = () => {
-    fetch("api/assignments", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-      method: "POST",
-    })
-      .then((response) => {
-        if (response.status === 200) return response.json();
-      })
-      .then((assignment) => {
-        window.location.href = `/assignments/${assignment.id}`;
-      });
+    ajax("api/assignments", "POST", jwt).then((assignment) => {
+      window.location.href = `/assignments/${assignment.id}`;
+    });
   };
 
   return (
